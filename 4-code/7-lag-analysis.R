9a632a4cf76fd35faa0f381e5cbaedc867820282
smkerr <steven.miles.k@gmail.com>
2024-04-25 23:01:52 +0200

    Add lag analysis summary table

--- 4-code/7-lag-analysis.R
+++ 4-code/7-lag-analysis.R
@@ -13,6 +13,7 @@ pacman::p_load(
   dplyr, 
   ggplot2, 
   glue, 
+  gt,
   here, 
   lubridate, 
   purrr, 
@@ -230,7 +231,88 @@ lag_results |>
 # Save image
 ggsave(filename = here("5-visualization/spearman-pvalues-barplot.png"), width = 10, height = 8, dpi = 300)
 
-# TODO: Create table
+# Summary Table ================================================================
+summary_data <- lag_results %>%
+  group_by(page_title) %>%
+  mutate(page_title = factor(page_title, levels = rev(order_by_coefficients))) |> 
+  summarise(
+    Avg_Estimate = mean(estimate, na.rm = TRUE),
+    Median_Estimate = median(estimate, na.rm = TRUE),
+    Avg_P_Value = mean(p.value, na.rm = TRUE),
+    Min_P_Value = min(p.value, na.rm = TRUE),
+    Significant_Lags = sum(p.value < 0.05, na.rm = TRUE)
+  )
+
+summary_table <- gt(summary_data) %>%
+  tab_header(
+    title = "Summary of Lag Analysis"
+  ) %>%
+  cols_label(
+    Avg_Estimate = "Average Estimate",
+    Median_Estimate = "Median Estimate",
+    Avg_P_Value = "Average P-Value",
+    Min_P_Value = "Minimum P-Value",
+    Significant_Lags = "Count of Significant Lags (p < 0.05)"
+  ) %>%
+  fmt_number(
+    columns = c(Avg_Estimate, Median_Estimate, Avg_P_Value, Min_P_Value),
+    decimals = 2
+  ) %>%
+  fmt_number(
+    columns = c(Significant_Lags)
+  ) %>%
+  tab_options(
+    table.font.size = px(12),
+    heading.background.color = "#D3D3D3"
+  )
+
+summary_table <- summary_table %>%
+  # Bold the page_title column
+  tab_style(
+    style = list(
+      cell_text(weight = "bold")
+    ),
+    locations = cells_body(columns = c(page_title))
+  ) %>%
+  # Bold the column headers
+  tab_style(
+    style = list(
+      cell_text(weight = "bold")
+    ),
+    locations = cells_column_labels(columns = everything())
+  ) %>%
+  # Bold the table title
+  tab_header(
+    title = md("**Summary of Lag Analysis**")
+  )
+
+
+summary_table <- summary_table %>%
+  data_color(
+    columns = c(Avg_Estimate, Median_Estimate),
+    colors = scales::col_numeric(
+      palette = c("lightblue", "darkblue"),
+      domain = NULL  # Auto-scale based on the data range of each column
+    )
+  ) %>%
+  data_color(
+    columns = c(Avg_P_Value, Min_P_Value),
+    colors = scales::col_numeric(
+      palette = c("lightgreen", "darkgreen"),
+      domain = NULL
+    )
+  ) %>%
+  data_color(
+    columns = c(Significant_Lags),
+    colors = scales::col_numeric(
+      palette = c("pink", "red"),
+      domain = NULL
+    )
+  )
+summary_table
+
+# Save the table as HTML
+gtsave(summary_table, here("5-visualization/lag-analysis-summary-table.html"))
 
 # Conclusion ===================================================================
 #> Negative lags correspond with the time-lag effect of independent vars on cases
