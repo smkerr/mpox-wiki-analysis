f6a1dc9109b8203a622bba7945b25aadaf1da82f
smkerr <steven.miles.k@gmail.com>
2024-04-28 09:59:41 +0200

    Add news coverage data

--- 4-code/2c-get-news-data.R
+++ 4-code/2c-get-news-data.R
@@ -16,7 +16,8 @@ pacman::p_load(
   lubridate,
   purrr,
   readr,
-  tibble
+  tibble,
+  tidyr
   )
 
 # Set API key
@@ -34,7 +35,7 @@ if (file.exists(here("3-data/mpox-news/mpox-total-articles.csv"))) {
 
   # Key parameters
   start_date <- as_date("2022-01-01") 
-  end_date <- as_date("2023-02-05")
+  end_date <- as_date("2022-11-27")
   date_sequence <- seq.Date(start_date, end_date, by = 1)
   
   for (search_query in search_terms) {
@@ -71,14 +72,12 @@ if (file.exists(here("3-data/mpox-news/mpox-total-articles.csv"))) {
 if (file.exists(here("3-data/mpox-news/mpox-article-headlines.csv"))) {
   headlines_df <- read_csv(here("3-data/mpox-news/mpox-article-headlines.csv"))  
 } else {
-  # Time period where "monkeypox" and "mpox" were both in use (training data)
-  
   # Initialize empty df to store data
   headlines_df <- data.frame()
 
   # Key parameters
   start_date <- as_date("2022-11-28")
-  end_date <- as_date("2023-02-05")
+  end_date <- as_date("2024-02-27")
   date_sequence <- seq.Date(start_date, end_date, by = 1)
 
   for (search_query in search_terms) {
@@ -107,48 +106,16 @@ if (file.exists(here("3-data/mpox-news/mpox-article-headlines.csv"))) {
       Sys.sleep(1)
     }
   }
-  
-  # Time period where "monkeypox" and "mpox" were both in use (test data)
-  # TODO: Combine this section with the section below
-  
-  # Key parameters
-  start_date <- as_date("2023-08-05") ### I ALREADY CHECKED THE DATES!!!
-  end_date <- as_date("2023-09-23") ### THESE ARE THE ONES YOU NEED TO DOWNLOAD NEXT
-  date_sequence <- seq.Date(start_date, end_date, by = 1)
-  
-  for (search_query in search_terms) {
-    for (date in date_sequence) {
-      # Construct API URL
-      url <- glue("https://gnews.io/api/v4/search?q={URLencode(search_query)}&lang=en&country=us&max=10&from={as_date(date)}T00:00:00Z&to={as_date(date)}T23:59:59Z&apikey={api_key}")
-      
-      # Perform the GET request
-      response <- GET(url)
-      if (status_code(response) == 200) {
-        parsed_content <- content(response, as = "parsed")
-        
-        # Create a data frame for the current date's articles
-        if (parsed_content$totalArticles > 0) {
-          articles_df <- parsed_content |> 
-            pluck("articles") |> 
-            map_df(unlist) |> 
-            mutate(search_term = search_query, publishedAt = ymd_hms(publishedAt))
-          
-          # Append to the main data frame
-          headlines_df <- bind_rows(headlines_df, articles_df)
-        }
-      }
-      # Respect API rate limits
-      Sys.sleep(1)
-    }
-  }
-  # save results
-  write_csv(headlines_df, here("3-data/mpox-news/mpox-article-headlines.csv"))
 }
 
 
 # Create de-duplicated news article count dataframe ============================
+# Full date range
+date_sequence <- seq.Date(from = as_date("2022-01-01"), to = as_date("2024-02-27"), by = 1) |> 
+  as_tibble_col(column_name = "date")
+
 news_deduplicated <- bind_rows(
-  news_df |> # news articles counts before name change
+  news_df |> # news article counts before name change
     filter(date < as_date("2022-11-28")) |> 
     select(-search_term),
   headlines_df |> # news article counts after name change
@@ -156,9 +123,9 @@ news_deduplicated <- bind_rows(
     distinct(date, title, description, content) |> # de-duplicate 
     count(date, name = "n_articles")
   ) |> 
+  right_join(date_sequence, by = join_by(date)) |> 
+  replace_na(list(n_articles = 0)) |> # impute zeroes for date without articles
   arrange(date)
-
-# TODO: Add zeroes for dates without articles
 
 # save results
 write_csv(news_deduplicated, here("3-data/mpox-news/mpox-total-articles-deduplicated.csv"))
